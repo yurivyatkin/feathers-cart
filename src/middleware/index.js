ce2527af12edede270905c9ee63aa1c9c55e3ad9
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2019-02-07 01:59:22 +1300

    Enable sessions in params for the cart path

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-module.exports = function (app) {
+module.exports = function(app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
+
+  // We store the cart information within the session.
+  // Doing the following we enable the session key in params
+  // within services and hooks:
+  app.use('/cart', (req, res, next) => {
+    req.feathers.session = req.session;
+    next();
+  });
 };
