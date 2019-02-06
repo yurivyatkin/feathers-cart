c5bd6fbc0aa40c037b5d840250e2317d38cd645f
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2019-02-07 03:10:57 +1300

    Implement basic cart's behaviour (POC)
    Can view the cart, add products by id (with quantity sent in the body), and delete products from the cart.

--- src/services/cart/cart.class.js
+++ src/services/cart/cart.class.js
@@ -4,34 +4,73 @@ class Service {
     this.options = options || {};
   }
 
+  // This should return the list of all items in the cart
   async find(params) {
-    return [];
+    const { session } = params;
+    const { cart = {} } = session;
+    return cart;
   }
 
+  // This should return the item with the given id from the cart
   async get(id, params) {
-    return {
-      id,
-      text: `A new message with ID: ${id}!`,
-    };
+    const { session } = params;
+    const { cart = {} } = session;
+    const { items = [] } = cart;
+    // _id is an object:
+    const product = items.find(item => item._id == id);
+    return product;
   }
 
-  async create(data, params) {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)));
-    }
+  // This should not be here %)
+  // async create(data, params) {
+  //   if (Array.isArray(data)) {
+  //     return Promise.all(data.map(current => this.create(current, params)));
+  //   }
 
-    return data;
-  }
+  //   return data;
+  // }
 
+  // This should add the item with the given id to the cart
+  // ... with the quantity taken from the data
+  // (this means that to add a product, the quantity must be supplied in the body)
   async update(id, data, params) {
+    const { session } = params;
+    const { cart = {} } = session;
+    // The product is added in a hook:
+    const product = params.product;
+    // TODO: don't duplicate products
+    const prevItems = cart.items || [];
+    const prodIdx = prevItems.findIndex(item => item._id == id);
+    // const items = [...(cart.items || []), product];
+    const newItem = Object.assign({}, product, { quantity: data.quantity });
+    const items = [
+      ...prevItems.slice(0, prodIdx),
+      newItem,
+      ...prevItems.slice(prodIdx + 1),
+    ];
+    cart.items = items;
+    session.cart = cart;
     return data;
   }
 
-  async patch(id, data, params) {
-    return data;
-  }
+  // This is not needed: update is enough
+  // async patch(id, data, params) {
+  //   return data;
+  // }
 
+  // This should delete the item with the given id from the cart
   async remove(id, params) {
+    const { session } = params;
+    const { cart = {} } = session;
+    const prevItems = cart.items || [];
+    // _id is an object:
+    const prodIdx = prevItems.findIndex(item => item._id == id);
+    const items = [
+      ...prevItems.slice(0, prodIdx),
+      ...prevItems.slice(prodIdx + 1),
+    ];
+    cart.items = items;
+    session.cart = cart;
     return { id };
   }
 }
