3fe23c9ca1f6acdea11d9db83a56b9cb9d9de21e
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2019-02-10 21:39:02 +1300

    Add test todos for the cart service... ... acording to the requirements.

--- test/services/cart.test.js
+++ test/services/cart.test.js
@@ -5,4 +5,13 @@ describe('\'cart\' service', () => {
     const service = app.service('cart');
     expect(service).toBeTruthy();
   });
+  it.todo('lists the items added to the cart');
+  it.todo('shows an individual item in the cart, given the product\'s id');
+  it.todo('adds one item to the list, if no quantity provided');
+  it.todo('adds a given quantity of items to the list');
+  it.todo('replaces the item with a given quantity, if the product with the given id is in the list');
+  it.todo('shows the cart total alongside the list');
+  it.todo('updates the cart total when an item is added');
+  it.todo('updates the cart total when an item is updated');
+  it.todo('presents all prices rounded up to 2 decimal places');
 });
